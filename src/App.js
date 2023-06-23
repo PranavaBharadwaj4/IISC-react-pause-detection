d3c73e34c77ceeee7c609369f8c8d5f84a475026
Pranava Bharadwaj <pranava@Pranavas-MacBook-Pro.local>
2023-06-23 21:49:22 +0530

    final reading

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@ import './App.css';
 import SpeechRecorder from './components/SpeechRecorder';
 import SpeechRecorderTwo from './components/SpeechRecorderTwo';
 import SendRecording from './components/SendRecording';
+import SpeechRecorderCopy from './components/SpeechRecorderCopy';
 function App() {
   return (
     <>
     <SpeechRecorderTwo />
     {/* <SpeechRecorder /> */}
+    {/* <SpeechRecorderCopy/> */}
 
     {/* <SendRecording /> */}
     </>
--- src/components/SpeechRecorderCopy.jsx
+++ src/components/SpeechRecorderCopy.jsx
@@ -1,9 +1,10 @@
 import React, { useEffect, useState } from 'react';
+// import {Chart} from 'chart.js'
 import { Chart, registerables} from 'chart.js';
 
 Chart.register(...registerables);
 
-const SpeechRecorder = () => {
+const SpeechRecorderCopy = () => {
   const [isSpeaking, setIsSpeaking] = useState(false);
   const [noiseLevel, setNoiseLevel] = useState(0);
   const [noiseLevels, setNoiseLevels] = useState([]);
@@ -13,7 +14,13 @@ const SpeechRecorder = () => {
   const [isPaused, setIsPaused] = useState(false);
   const [chart, setChart] = useState(null);
   const [dataG, setDataG] = useState(null);
-  const windowSize = 100; // Adjust the window size as per your needs
+  const [length, setLength] = useState(1000);
+  const [noiseObject, setNoiseObject] = useState({
+    noiseLevel: 0,
+    noiseLevels: [],
+    noiseLevelsTotal: []
+  })
+  const windowSize = 200; // Adjust the window size as per your needs
   
   
 
@@ -42,6 +49,7 @@ const SpeechRecorder = () => {
   };
   // let chart = null;
 
+
   useEffect(() => {
     const detectSilence = (stream, onSoundEnd, onSoundStart, silenceDelay, minDecibels) => {
       console.log("called")
@@ -55,10 +63,19 @@ const SpeechRecorder = () => {
       const dataFFD = new Uint8Array(analyser.frequencyBinCount);
       let silenceStart = performance.now();
       let triggered = true;
+      
+      // if(isPaused){
+      //   setDataG(noiseAverage)
+
+      //   chart.update()
+      // }
       //  setDataG(isPaused? noiseLevelsTotal : noiseLevels);
 
 
       // Create the chart using the canvas element
+
+//////////////////////////////////////////////////////Graph Settings ////////////////////////////////////////////////////////////////////////
+
     const ctxChart = document.getElementById('noiseGraph').getContext('2d');
     let xchart = new Chart(ctxChart, {
       type: 'line',
@@ -68,19 +85,16 @@ const SpeechRecorder = () => {
         datasets: [
           {
             label: 'Noise Level',
-            data: noiseLevels,
+            data: dataG,
             borderColor: 'rgba(75, 192, 192, 1)',
             borderWidth: 1,
-            fill: true,
+            fill: false,
           },
         ],
       },
       options: {
         responsive: true,
         maintainAspectRatio: false,
-        animation: {
-          duration: 0 // Disable chart animations
-        },
         scales: {
           y: {
             suggestedMin: 0,
@@ -113,6 +127,7 @@ const SpeechRecorder = () => {
 
 
       function loop(time) {
+        console.log(length)
         requestAnimationFrame(loop);
         analyser.getByteFrequencyData(data);
 
@@ -120,16 +135,36 @@ const SpeechRecorder = () => {
 
         // Calculate noise level as the average of all frequency data values
         const average = Array.from(data).reduce((sum, value) => sum + value, 0) / data.length;
-        setNoiseLevel(average);
-        if(!triggered){
-            console.log("total pause")
-          setNoiseLevelsTotal(prevLevels => [...prevLevels, average]); // Add the noise level to the array
-        }
-        setNoiseLevels(prevLevels => {
-          const updatedLevels = [...prevLevels, average].slice(-windowSize); // Maintain the window size
+        // setNoiseLevel(average);
+        // // setNoiseLevelsTotal(prevLevels => [...prevLevels, average]); // Add the noise level to the array
+        // setNoiseLevelsTotal(prevLevels => {
+        //   const updatedLevels = [...prevLevels, average]; // Maintain the window size
+        //   setNoiseAverage(calculateAverage(updatedLevels)); // Update the average based on the updated window
+        //   return updatedLevels;
+        // });
+        // setNoiseLevels(prevLevels => {
+        //   const updatedLevels = [...prevLevels, average].slice(-windowSize); // Maintain the window size
+        //   setNoiseAverage(calculateAverage(updatedLevels)); // Update the average based on the updated window
+        //   return updatedLevels;
+        // });
+        setNoiseObject((noiseObject)=>({...noiseObject,
+           noiseLevel: average,
+           noiseLevelsTotal: ()=>{
+              const updatedLevels = [...noiseObject.noiseLevels, average]; // Maintain the window size
+              console.log("hiii")
+          return updatedLevels;
+           },
+           noiseLevels: ()=>{
+              const updatedLevels = [...noiseObject.noiseLevels, average].slice(-windowSize); // Maintain the window size
           setNoiseAverage(calculateAverage(updatedLevels)); // Update the average based on the updated window
           return updatedLevels;
-        });
+           },
+
+           
+           
+        }));
+        console.log(noiseObject.noiseLevelsTotal)
+        console.log(noiseObject.noiseLevels)
 
         if (data.some((v) => v)) {
           if (triggered) {
@@ -187,38 +222,32 @@ const SpeechRecorder = () => {
     };
   }, []);
 
-  //Chart data setting
+  /////////////////////////////////////////////////////Chart UseEffect //////////////////////////////////////////////////////////////
+ 
   useEffect(() => {
-    
-    if(!isPaused){
-      console.log("noiseLevelsTotal")
-      setNoiseLevelsTotal([]);
-    }
-    console.log("chart initialization")
-    console.log(chart)
-  }, [isPaused]);
-  
-  
-  useEffect(() => {
+    // Update the chart with the new noise levels
+    // console.log(chart)
     if (chart) {
-      const dataset = chart.data.datasets[0];
-      
-      const updatedData = isPaused ? noiseLevelsTotal : noiseLevels;
-      
-      // Update the chart's data and labels
-      dataset.data = updatedData;
-      chart.data.labels = Array.from({ length: updatedData.length }).fill('');
-  
-      // Set the suggested maximum value for the y-axis based on the maximum noise level
-      // const maxNoiseLevel = Math.max(...updatedData);
-      // chart.options.scales.y.suggestedMax = Math.ceil(maxNoiseLevel / 10) * 10;
-  
-      // Update the chart
+      if(isPaused){
+        setDataG(noiseLevelsTotal);
+        setLength(200);
+      chart.data.datasets[0].data = noiseObject.noiseLevelsTotal;
       chart.update();
-    }
-  }, [chart, noiseLevels, noiseLevelsTotal, isPaused]);
-  
+      
 
+      }else{
+        setLength(200);
+        setDataG(noiseAverage)
+        console.log("hi")
+        chart.data.datasets[0].data = noiseObject.noiseLevels;
+        // console.log(chart.data.datasets[0].data)
+        chart.update();
+      }
+
+    }
+  }, [noiseLevels]);
+
+  
 
 
 
@@ -229,6 +258,7 @@ const SpeechRecorder = () => {
     const sum = values.reduce((total, level) => total + level, 0);
     return sum / values.length;
   };
+
 
 
   useEffect(() => {
@@ -254,7 +284,7 @@ const SpeechRecorder = () => {
           <>
           <div className={`indicator ${isSpeaking ? 'speaking' : 'silence'}`} />
           <div className="average">Noise Average: {noiseAverage.toFixed(2)}</div>
-          <div>Noise Level: {noiseLevel.toFixed(2)}</div>
+          <div>Noise Level: {noiseObject.noiseLevel.toFixed(2)}</div>
           <div>Audio Category: {audioCategory}</div>
           </>
         )}
@@ -267,4 +297,4 @@ const SpeechRecorder = () => {
   );
 };
 
-export default SpeechRecorder;
+export default SpeechRecorderCopy;
